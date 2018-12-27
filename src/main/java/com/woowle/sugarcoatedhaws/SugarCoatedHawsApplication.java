420c7185e1611113d28d55cefc5a2e3f7d0dc1cd
夏建高 <jiangao.xia.o>
2018-12-27 16:24:49 +0800

    整合eureka

--- src/main/java/com/woowle/sugarcoatedhaws/SugarCoatedHawsApplication.java
+++ src/main/java/com/woowle/sugarcoatedhaws/SugarCoatedHawsApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 
+@EnableEurekaClient
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class,scanBasePackages = "com.woowle.sugarcoatedhaws")
 @MapperScan("com.woowle.sugarcoatedhaws.mapper")
 @EnableCaching
